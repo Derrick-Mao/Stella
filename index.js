5ef322527b1000744607f122ce4f009e9aac157c
Derrick Mao <derrickmao03@gmail.com>
2024-02-08 12:39:21 -0800

    conversation array w/ prev msgs 10

--- index.js
+++ index.js
@@ -21,20 +21,46 @@ client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
     if (!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) return;
 
+    let conversation = [];
+    conversation.push({
+        role: 'system',
+        content: 'Chat GPT is a friendly chatbot.'
+    });
+
+    let prevMsgs = await message.channel.messages.fetch({ limit: 10});
+    prevMsgs.reverse();
+
+    prevMsgs.forEach((msg) => {
+        if (msg.author.bot && msg.author.id !== client.user.id) return;
+
+        const username = msg.author.username.replace(/\s+/g, '_').replace(/[^\w\s]/gi, '');
+
+        if (msg.author.id === client.user.id) {
+            conversation.push({
+                role: 'assistant',
+                name: username,
+                content: msg.content,
+            });
+
+            return;
+        }
+
+        conversation.push({
+            role: 'user',
+            name: username,
+            content: msg.content,
+        })
+    })
+
     const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
-        messages: [
-            {
-                role: 'system',
-                content: 'Chat GPT is a friendly chatbot.'
-            },
-            {
-                role: 'user',
-                content: message.content,
-            },
-        ],
+        messages: conversation,
     })
     .catch((error) => console.error('OpenAI Error:\n', error));
+
+    if (!response) {
+        message.reply('Trouble with OpenAI API. Try Again.');
+    }
 
     message.reply(response.choices[0].message.content);
 });
