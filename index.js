c33500d5ce47d6d06d322b0fa7231714d2123f5b
Derrick Mao <derrickmao03@gmail.com>
2024-02-08 12:00:27 -0800

    openai msgCr

--- index.js
+++ index.js
@@ -7,16 +7,36 @@ const client = new Client({
     intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent']
 });
 
-client.on('read', () => {
-    console.login('The bot is online.')
+client.on('ready', () => {
+    console.log('The bot is online.')
 });
 
 const CHANNELS = ['1205114084157169664'];
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY
+});
 
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
     if (!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) return;
 
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'system',
+                content: 'Chat GPT is a friendly chatbot.'
+            },
+            {
+                role: 'user',
+                content: message.content,
+            },
+        ],
+    })
+    .catch((error) => console.error('OpenAI Error:\n', error));
+
+    message.reply(response.choices[0].message.content);
 });
 
 client.login(process.env.TOKEN);
