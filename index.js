aca0ee9592ddc9b50c8554a7b06b11ed532175a3
Derrick Mao <derrickmao03@gmail.com>
2024-02-08 03:53:26 -0800

    client

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 require('dotenv/config');
 
+const { Client } = require('discord.js');
+const { OpenAI } = require('openai');
 
+const client = new Client({
+    intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent']
+});
+
+client.on('read', () => {
+    console.login('The bot is online.')
+});
+
+const CHANNELS = ['1205114084157169664'];
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) return;
+
+});
+
+client.login(process.env.TOKEN);
